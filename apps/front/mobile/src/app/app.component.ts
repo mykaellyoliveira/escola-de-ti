3ea0e0e5512eb2863aa1d723c3ebd60a90759a29
Gui Seek <guiseek@gmail.com>
2023-07-24 20:19:34 -0300

    feat: 🎸 add angular mobile app

---
+++ apps/front/mobile/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'escola-de-ti-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'front-mobile';
+}
