1241c6462d94fe542d3b508e56574b2ab9893214
Gui Seek <guiseek@gmail.com>
2023-07-25 14:43:47 -0300

    feat: 🎸 front auth form

--- apps/front/mobile/src/app/app.component.ts
+++ apps/front/mobile/src/app/app.component.ts
@@ -1,19 +1,41 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+export class AuthForm extends FormGroup {
+  constructor() {
+    super({
+      username: new FormControl(''),
+      password: new FormControl(''),
+    })
+  }
+}
 
 @Component({
   selector: 'escola-de-ti-root',
   template: `
-    <pre>
-      {{ hello$ | async | json }}
-    </pre>
+    <form [formGroup]="form" (submit)="onSubmit()">
+      <label>
+        <input type="text" formControlName="username" >
+      </label>
+      <label>
+        <input type="password" formControlName="password" >
+      </label>
+
+      <button>Login</button>
+    </form>
   `,
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'front-mobile';
 
   httpClient = inject(HttpClient)
 
-  hello$ = this.httpClient.get('/api')
+  form = new AuthForm()
+
+  onSubmit() {
+    this.httpClient
+      .post('/api/auth/login', this.form.value)
+      .subscribe(console.log)
+  }
 }
--- apps/front/mobile/src/app/app.module.ts
+++ apps/front/mobile/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
